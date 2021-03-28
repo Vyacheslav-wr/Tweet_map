1d989b7ef7286f4a18f3e69575c42ebbed8fb1e0
Salei Vyacheslav <65761477+Vyacheslav-wr@users.noreply.github.com>
2021-03-28 16:50:02 +0300

    Add files via upload

--- JavaGraphic/src/sample/Main.java
+++ JavaGraphic/src/sample/Main.java
@@ -1,25 +1,37 @@
 package sample;
 
+import BuisnessLogic.Tweet;
 import BuisnessLogic.TweetParse;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.geometry.Insets;
+import javafx.scene.*;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
 public class Main extends Application {
-
+    double cenX = 518.5;
+    double cenY = 358;
+    double entX;
+    double entY;
+    double xOffset;
+    double yOffset;
+    final public double camereq = 100.0;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Чаво я наделал");
+        primaryStage.setTitle("USA MAP");
+        Scene scene = new Scene(root, 1037, 716, Color.TURQUOISE);
+        primaryStage.setScene(scene);
         primaryStage.sizeToScene();
-        primaryStage.setScene(new Scene(root, 1037, 716));
         primaryStage.show();
-        TweetParse.TweetParsing();
-
     }
-
 
     public static void main(String[] args) {
         launch(args);
